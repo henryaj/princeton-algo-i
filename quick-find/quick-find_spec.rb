2961ae55e74144a3bd3cc8f9dd36bd8d4790d219
Henry Stanley <hstanley@pivotal.io>
2016-02-04 18:57:53 +0000

    Neaten tests

--- quick-find/quick-find_spec.rb
+++ quick-find/quick-find_spec.rb
@@ -1,10 +1,10 @@
 require_relative "./quick-find"
 
 RSpec.describe QuickFind do
+  let(:qf) { QuickFind.new(10) }
+
   describe "#new" do
     it "initialises an array with id equal to the array index" do
-      qf = QuickFind.new(10)
-
       (0..9).each do |i|
         expect(qf.data[i]).to eq i
       end
@@ -13,7 +13,6 @@ RSpec.describe QuickFind do
 
   describe "#union" do
     it "connects two components" do
-      qf = QuickFind.new(10)
       qf.union(2, 10)
 
       expect(qf.data[2]).to eq(10)
@@ -22,7 +21,6 @@ RSpec.describe QuickFind do
 
   describe "#find" do
     it "returns true if the elements are connected" do
-      qf = QuickFind.new(10)
       qf.union(2, 10)
 
       expect(qf.find(2, 10)).to be true
